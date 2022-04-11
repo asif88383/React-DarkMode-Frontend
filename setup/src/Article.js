e1ec408325c3426747b2ba2007f10321834a0ed0
Muhammad Asif <asif88383@gmail.com>
2022-04-11 23:03:33 +0500

    first commit

--- setup/src/Article.js
+++ setup/src/Article.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
+
+const Article = ({title, snippet, date, length}) => { // Article is a component that takes in a title, a snippet, a date, and a length  as props  and returns JSX
+  return (
+    <article className='post'>
+      <h2>{title}</h2>
+      <div className='post-info'>
+        <span>{moment(date).format('dddd Do, YYYY')}</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>  // snippet is a prop that is passed in as a prop
+    </article>
+  )
 }
 
 export default Article
